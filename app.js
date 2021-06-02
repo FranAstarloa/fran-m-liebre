f9003cdedc67ff01a525f186b7da29c36ace835d
FranAstarloa <astarloafrancisco@gmail.com>
2021-06-02 14:59:05 -0300

    final ml

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.use(express.static(path.resolve(__dirname, "./public")));
 
 app.set("port", process.env.PORT || 3000)
 
-app.listen(app.get("port"),() => console.log("Server Start"))
+app.listen(app.get("port"),() => console.log("Server Start http://localhost:"+app.get("port")))
 
 app.get("/",(req,res)=> res.sendFile(path.resolve(__dirname, "./views", "home.html")));
 
